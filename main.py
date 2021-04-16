e255257c4661ba171d907a5e59eedf6dc7096a14
Sungjae Lee <sungjae@Sungjaes-MacBook-Pro.local>
2021-04-16 19:43:28 +0900

    test

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from flask import render_template, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
 import os
-import json
-
 import requests
 
 UPLOAD_FOLDER = 'uploads'
@@ -18,9 +16,24 @@ app.secret_key = "secret key"
 def index():
     return render_template('index.html')
 
-@app.route('/<name>')
-def hello_name(name):
-    return f"Hello {name}!"
+@app.route('/', methods=['POST'])
+def submit_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            label, acc = [['dog']], [[99.99]]
+            flash(label)
+            flash(acc)
+            flash(filename)
+            return redirect('/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80 ,debug=True)
