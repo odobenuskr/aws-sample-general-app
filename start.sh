a97e9dc60341abfb1c8d666b6cdb0c582df7475e
Sungjae Lee <odobenuseKR@gmail.com>
2021-04-18 16:25:49 +0900

    install mysql

--- start.sh
+++ start.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 apt-get update
 apt-get install python3-pip -y
+apt-get install mysql-server
+apt-get install libmysqlclient-dev
 pip3 install virtualenv
+
 cd /home/ubuntu
 git clone https://github.com/odobenuskr/aws-sample-general-app
 cd aws-sample-general-app
 mkdir uploads
+
 virtualenv venv
 . venv/bin/activate
 pip3 install -r requirements.txt
+
 cp main.service /etc/systemd/system/
 systemctl start main
 systemctl enable main
