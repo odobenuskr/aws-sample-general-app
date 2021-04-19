77e0f4d1bb95790e06f4030c7e4c1593cdc1b33f
Sungjae Lee <sungjae@Sungjaes-MacBook-Pro.local>
2021-04-19 14:39:04 +0900

    update

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def mysql_hello():
 
 @app.route('/uploads/<filename>')
 def display_image(filename):
-	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+	return redirect(url_for('uploads', filename=filename), code=301)
 
         
 if __name__ == '__main__':
